2d3553b7832782bccadd0639a9fe95bf9ebff33c
karim216 <njigbetnkom@gmail.com>
2022-12-30 08:56:15 +0100

    remove commented codes in index.js

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// const numbers = [1, 2, 3, 4]
-// const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8]
-// const numbersCount = numbers.length
-
-// let index = 0
-// let sum = 0
-
-// //boucle sum table items elements
-// while (index < numbersCount) {
-//     const number = numbers[index]
-
-//     sum = sum + number
-
-//     index = index + 1
-// }
-
-// console.log(`Sum is ${sum}`)
-
-// Get the current year for the copyright
-
 console.log("test exect file")
 
 $('#year').text(new Date().getFullYear());
