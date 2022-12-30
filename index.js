e9a900b51e231d76cb38d18d70b05551bf530f95
karim216 <njigbetnkom@gmail.com>
2022-12-30 08:50:30 +0100

    update navbar css, index.js & index reorg

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const numbers = [1, 2, 3, 4]
-const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8]
-const numbersCount = numbers.length
+// const numbers = [1, 2, 3, 4]
+// const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8]
+// const numbersCount = numbers.length
 
-let index = 0
-let sum = 0
+// let index = 0
+// let sum = 0
 
-//boucle sum table items elements
-while (index < numbersCount) {
-    const number = numbers[index]
+// //boucle sum table items elements
+// while (index < numbersCount) {
+//     const number = numbers[index]
 
-    sum = sum + number
+//     sum = sum + number
 
-    index = index + 1
-}
+//     index = index + 1
+// }
 
-console.log(`Sum is ${sum}`)
+// console.log(`Sum is ${sum}`)
+
+// Get the current year for the copyright
+
+console.log("test exect file")
+
+$('#year').text(new Date().getFullYear());
+
+// Init Scrollspy
+$('body').scrollspy({ target: '#main-nav' });
+
+// Smooth Scrolling
+$("#main-nav a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+
+            window.location.hash = hash;
+        });
+    }
+});
