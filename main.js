c53a6955796d8c35df720459fcdc8621bc7bdc7d
karim216 <njigbetnkom@gmail.com>
2023-01-03 05:57:07 +0100

    add hook test package update main.js

--- main.js
+++ main.js
@@ -3,7 +3,25 @@
 /* ************************************** CODE PRINCIPAL *************************************** */
 /*************************************************************************************************/
 
-$(document).ready(function(){
+$(document).ready(function () {
+    $('#year').text(new Date().getFullYear());
+
+    // Init Scrollspy
+    $('body').scrollspy({ target: '#main-nav' });
+
+    // Smooth Scrolling
+    $("#main-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
     console.log('Je signe sur le commit de Karim')
 
 })
